56d5fbd154689aa603b52b3c5030b5147d924a75
LukaszZAtos <126456806+LukaszZAtos@users.noreply.github.com>
2023-05-30 16:33:11 +0200

    Create chk_ansible.go
    initial version

---
+++ chk_ansible.go
@@ -0,0 +1,124 @@
+package main
+
+import (
+	"encoding/json"
+	"fmt"
+	"io/ioutil"
+	"net/http"
+	"os"
+)
+
+type AnsibleJob struct {
+	ID        int    `json:"id"`
+	Status    string `json:"status"`
+	Started   string `json:"started"`
+	Finished  string `json:"finished"`
+	Created   string `json:"created"`
+	Modified  string `json:"modified"`
+	Failed    bool   `json:"failed"`
+	FailedMsg string `json:"failed_msg"`
+}
+
+func main() {
+	if len(os.Args) < 4 {
+		fmt.Println("Usage: go run monitor_ansible.go <AnsibleTowerURL> <AnsibleTokenFile> <AnsibleJobTemplate>")
+		os.Exit(1)
+	}
+
+	// Read command-line arguments
+	ansibleTowerURL := os.Args[1]
+	ansibleTokenFile := os.Args[2]
+	ansibleJobTemplate := os.Args[3]
+
+	// Read Ansible Tower token from file
+	ansibleToken, err := readTokenFromFile(ansibleTokenFile)
+	if err != nil {
+		fmt.Printf("Failed to read Ansible token: %v\n", err)
+		os.Exit(1)
+	}
+
+	// Check Ansible Tower health
+	if err := checkAnsibleHealth(ansibleTowerURL, ansibleToken); err != nil {
+		fmt.Printf("Failed to check Ansible Tower health: %v\n", err)
+		os.Exit(1)
+	}
+
+	// Get the job details
+	job, err := getAnsibleJob(ansibleTowerURL, ansibleToken, ansibleJobTemplate)
+	if err != nil {
+		fmt.Printf("Failed to get job details: %v\n", err)
+		os.Exit(1)
+	}
+
+	// Print the job status
+	fmt.Printf("Job ID: %d\n", job.ID)
+	fmt.Printf("Status: %s\n", job.Status)
+	fmt.Printf("Started: %s\n", job.Started)
+	fmt.Printf("Finished: %s\n", job.Finished)
+	fmt.Printf("Failed: %t\n", job.Failed)
+	if job.Failed {
+		fmt.Printf("Failure Message: %s\n", job.FailedMsg)
+		os.Exit(1)
+	}
+
+	os.Exit(0)
+}
+
+func readTokenFromFile(filename string) (string, error) {
+	tokenBytes, err := ioutil.ReadFile(filename)
+	if err != nil {
+		return "", err
+	}
+	return string(tokenBytes), nil
+}
+
+func checkAnsibleHealth(ansibleTowerURL, ansibleToken string) error {
+	client := &http.Client{}
+
+	req, err := http.NewRequest("GET", fmt.Sprintf("%s/ping/", ansibleTowerURL), nil)
+	if err != nil {
+		return err
+	}
+
+	req.Header.Add("Authorization", fmt.Sprintf("Bearer %s", ansibleToken))
+	resp, err := client.Do(req)
+	if err != nil {
+		return err
+	}
+	defer resp.Body.Close()
+
+	if resp.StatusCode != http.StatusOK {
+		return fmt.Errorf("Ansible Tower is not healthy (status code: %d)", resp.StatusCode)
+	}
+
+	return nil
+}
+
+func getAnsibleJob(ansibleTowerURL, ansibleToken, ansibleJobTemplate string) (*AnsibleJob, error) {
+	client := &http.Client{}
+
+	req, err := http.NewRequest("GET", fmt.Sprintf("%s/job_templates/%s/last_job/", ansibleTowerURL, ansibleJobTemplate), nil)
+	if err != nil {
+		return nil, err
+	}
+
+	req.Header.Add("Authorization", fmt.Sprintf("Bearer %s", ansibleToken))
+	resp, err := client.Do(req)
+	if err != nil {
+		return nil, err
+	}
+	defer resp.Body.Close()
+
+	body, err := ioutil.ReadAll(resp.Body)
+	if err != nil {
+		return nil, err
+	}
+
+	var job AnsibleJob
+	err = json.Unmarshal(body, &job)
+	if err != nil {
+		return nil, err
+	}
+
+	return &job, nil
+}
