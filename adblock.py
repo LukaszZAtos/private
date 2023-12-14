fdfe2900af20336e6faedfc1d78282d3a859a738
LukaszZAtos <126456806+LukaszZAtos@users.noreply.github.com>
2023-12-14 10:32:06 +0100

    Update adblock.py
    v2

--- adblock.py
+++ adblock.py
@@ -1,31 +1,33 @@
-from scapy.all import sniff, IP, UDP, DNS, DNSQR
-import re
+from scapy.all import sniff, DNS, DNSQR
 import subprocess
+import os
+
+# Configuration
+VPN_INTERFACE = 'tun0'  # Replace with your VPN interface
+DOMAINS_TO_BLOCK = ['googlevideo.com', 'anotherdomain.com']  # List of domains to block
+BLOCKLIST_PATH = '/etc/dnsmasq.blocklist'  # Path to dnsmasq blocklist
+
+def is_subdomain(domains, subdomain):
+    return any(subdomain.endswith('.' + domain) or subdomain == domain for domain in domains)
+
+def update_dnsmasq_blocklist(subdomain):
+    with open(BLOCKLIST_PATH, 'a+') as file:
+        file.seek(0)
+        if f"address=/{subdomain}/" not in file.read():
+            file.write(f"address=/{subdomain}/\n")
+            subprocess.run(["sudo", "systemctl", "restart", "dnsmasq"], check=True)
+            print(f"Blocked {subdomain}")
 
 def dns_query(packet):
-    if packet.haslayer(DNS) and packet.getlayer(DNS).qr == 0:  # DNS query
-        domain = str(packet[DNSQR].qname)
-        if 'pattern_of_interest' in domain:  # Check for your domain pattern
-            update_dnsmasq_blocklist(domain)
+    if packet.haslayer(DNS) and packet.getlayer(DNS).qr == 0:
+        domain_name = str(packet[DNSQR].qname.decode('utf-8')).rstrip('.')
+        if is_subdomain(DOMAINS_TO_BLOCK, domain_name):
+            print(f"Detected DNS query for {domain_name}")
+            update_dnsmasq_blocklist(domain_name)
 
-def update_dnsmasq_blocklist(domain):
-    blocklist_path = "/etc/dnsmasq.blocklist"  # Path to your dnsmasq blocklist file
-    block_entry = f"address=/{domain}/#\n"     # Format for dnsmasq blocklist entry
+# Make sure the dnsmasq blocklist file exists
+os.makedirs(os.path.dirname(BLOCKLIST_PATH), exist_ok=True)
+open(BLOCKLIST_PATH, 'a').close()
 
-    try:
-        # Check if the domain is already in the blocklist
-        with open(blocklist_path, 'r+') as file:
-            if block_entry not in file.read():
-                file.write(block_entry)
-                print(f"Added {domain} to blocklist.")
-
-        # Restart dnsmasq to apply the changes
-        subprocess.run(["sudo", "systemctl", "restart", "dnsmasq"], check=True)
-        print("dnsmasq restarted successfully.")
-    except Exception as e:
-        print(f"Error updating dnsmasq blocklist: {e}")
-    # Function to update dnsmasq blocklist
-    # This involves file operations and possibly executing system commands
-
-# Start sniffing on VPN interface (replace 'vpn_interface_name' with your actual VPN interface)
-sniff(iface='tun0', filter='tcp port 443', prn=dns_query)
+# Start packet sniffing
+sniff(iface=VPN_INTERFACE, filter="port 53", prn=dns_query)
